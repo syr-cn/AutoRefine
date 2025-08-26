bd1aedab52cffed661bd7df276a9c6a1f56d0907
shiyaorui <yaoruishi@gmail.com>
2025-08-26 20:17:26 +0800

    upload searchr1 prompt prefix as baseline

--- preprocess/data_process/utils.py
+++ preprocess/data_process/utils.py
@@ -8,6 +8,12 @@ If you identify missing knowledge, you may issue a search request using <search>
 After each search, you need to summarize and refine the existing documents in <refine> and </refine>. \
 You may send multiple search requests if needed. \
 Once you have sufficient information, provide a concise final answer using <answer> and </answer>. For example, <answer> Donald Trump </answer>. Question: {question}\n"""
+    elif template_type == 'searchr1':
+        prefix = f"""Answer the given question. \
+You must conduct reasoning inside <think> and </think> first every time you get new information. \
+After reasoning, if you find you lack some knowledge, you can call a search engine by <search> query </search> and it will return the top searched results between <information> and </information>. \
+You can search as many times as your want. \
+If you find no further external knowledge needed, you can directly provide the answer inside <answer> and </answer>, without detailed illustrations. For example, <answer> Beijing </answer>. Question: {question}\n"""
     else:
         raise NotImplementedError
     return prefix
